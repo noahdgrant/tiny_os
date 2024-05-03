61108f7ff9046c17089309641c1509ce788d6a4f
Noah Grant <noahdgrant@gmail.com>
2024-05-03 16:55:10 -0400

    feat(gdt): Add Global Descriptor Table to handle double faults
    Add a Global Descriptor Table (GDT) to handle double fault exceptions. Adding
    the GDT prevents a triple fault from occuring which would reset the hardware.
    
    Blog post 6

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,6 +2,8 @@ use crate::println;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
+use crate::gdt;
+
 // TODO: Implement the other CPU exceptions.
 
 // TODO: Look into writing my own code to handle exceptions and making my own
@@ -11,6 +13,11 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
         idt
     };
 }
@@ -21,6 +28,13 @@ pub fn idt_init() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 #[test_case]
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,14 @@
 
 use core::panic::PanicInfo;
 
+pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
 /// Central place for initialization routines.
 pub fn init() {
+    gdt::init();
     interrupts::idt_init();
 }
 
